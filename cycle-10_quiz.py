ff133a12d93fdedc9d45bc7d44eaaae7816ad141
p22mbonilla <p22mbonilla@fairfieldprep.org>
2022-02-02 10:19:44 -0500

    quiz

---
+++ cycle-10_quiz.py
@@ -0,0 +1,49 @@
+# Author MB 02/02/2022
+
+# the price average finder
+def funtion(i, v):
+    prices = [30, 40, 25, 55, 60, 80, 65]
+    v = prices
+        if i, v in enumerate(prices):
+            i = v / 2
+        return i
+
+# list same price but decreased by 5
+def sale(i, v):
+    prices = [30, 40, 25, 55, 60, 80, 65]
+    v = prices
+        if i, v in enumerate(prices):
+            i = v - 5
+        return i
+
+# income of the Prep pride store
+def income(prices, sales):
+    prices = [30, 40, 25, 55, 60, 80, 65]
+    sales = [1, 3, 2, 5, 2, 1, 2]
+        if prices, sales in enumerate(prices):
+            inocme =  prices / sales
+        return inocme
+
+# list of priuces and what is over 40
+def prices_over():
+    prices = [30, 40, 25, 55, 60, 80, 65]
+     if prices % 2 == 0:
+        break
+    else:
+        print(prices)
+
+
+
+
+
+print(funtion())
+print(sale())
+print(income())
+
+
+
+
+
+
+
+items = [['tee-shirt','long-sleeved','tanktop'],['quarter-zip','pullover','full-zip',half-zip]]
